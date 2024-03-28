313b58a4a0ccd757bd42cf3e4b0e365b535e3ef5
Evan Fathoni <90128256+Evan2205@users.noreply.github.com>
2024-03-29 05:11:47 +0700

    Add files via upload

---
+++ app/views/partials/appfooter.php
@@ -0,0 +1,16 @@
+<footer class="footer border-top">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="copyright">GS Company | &copy; <?php echo SITE_NAME ?> - <?php echo date('Y') ?></div>
+			</div>
+			<div class="col">
+				<div class="footer-links text-right">
+					<a href="<?php print_link('info/about'); ?>"></a> | 
+					
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</footer>
---
+++ app/views/partials/home/index.php
@@ -0,0 +1,138 @@
+<?php 
+$page_id = null;
+$comp_model = new SharedController;
+$current_page = $this->set_current_page_link();
+?>
+<div>
+    <div  class="mb-3">
+        <div class="container p-3">
+            <div class="row py-5">
+                <div class="col-md-6 comp-grid">
+                    <div class=""><div>
+                        <h1 class="display-4 bold mb-3">Global Solution</h1>
+                        <h4 class="mb-4">Welcome To Global Solution, Solution For You</h4>
+                        <div class="font-nm mb-4">
+                            Global Solution Bengkulu </>.
+                            Jasa pembuatan website dan aplikasi tanpa ribet dengan harga terjangkau.
+                        </div>
+                        <div class="">
+                            <a href=""><button class="btn btn-lg rounded btn-primary">Selengkapnya></button></a>
+                            <button class="btn btn-lg rounded btn-outline-primary">Lets Go</button>
+                        </div>
+                    </div></div>
+                </div>
+                <div class="col-md-6 comp-grid">
+                    <div class=""><div>
+                        <img src="assets/images/logoGS.jpg" heigth="80%" width="80%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div  class="bg-primary">
+        <div class="container">
+            <div class="row py-3">
+                <div class="col-md-12 comp-grid">
+                    <div class=""><div class="col-12 row"
+                        <div class="col-12 col-lg-4">
+                        <div class="d-inline p-3">
+                        </div>
+                        <div >
+                            <p class=" text-center"></p>
+                        </div>
+                    </div>
+                </div>  </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div  class="bg-light">
+    <div class="container p-3">
+        <div class="row py-5">
+            <div class="col-md-12 comp-grid">
+                <div class=""><div class="col-12 my-5">
+                    <h4 class="display-4 bold mb-3 text-center">Project Global Solution Bengkulu</h4>
+                    <h4 class="mb-4 text-center">Adapun salahsatu project yang dibuat oleh developer Global Solution Bengkulu yaitu SIKLINIK</h4>
+                    <img src="assets/images/siklinik.PNG" heigth="100%" width="100%">
+                        <p class="text-center font-nm">
+                        </p>
+                    </div></div>
+                </div>
+                <div class="col-md-12 comp-grid">
+                    <div class=""><div class="col-12 row mt-4">
+                        <div class="col-6 col-md-4 col-lg-3 mb-4">>
+                            <div class="mt-3 mb-4">
+                                <img src="assets/images/klinik.png" heigth="200" width="100">
+                                </div>
+                                <div class="bold font-nm mb-3">Web Sistem Informasi Klinik</div>
+                                <div class="mb-4">
+                                    Web Sistem Informasi Klinik Atau SIKLINIK salah satu sistem yang sudah dibuat
+                                    untuk sistem manajemen informasi klinik. Menu pada sistem ini yaitu Home, Apotik, Kasir & Laboratorium.
+                                </div>
+                                <div class="">
+                                    <button class="btn btn-outline-warning">Learn More</button>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-3 mb-4">>
+                                <div class="mt-3 mb-4">
+                                    <img src="assets/images/kasir.png" heigth="100" width="100">
+                                    </div>
+                                    <div class="bold font-nm mb-3">Sistem Kasir atau Keuangan</div>
+                                    <div class="mb-4">
+                                        Sistem kasir bisa buat dengan sistem aplikasi atau website sesuai dengan permintaan client.
+                                        Item yang terdapat didalamnya yaitu laporan kas masuk & keluar, laporan penjualan dll. 
+                                    </div>
+                                    <div class="">
+                                        <button class="btn btn-outline-warning">Learn More</button>
+                                    </div>
+                                </div>
+                                <div class="col-6 col-md-4 col-lg-3 mb-4">>
+                                    <div class="mt-3 mb-4">
+                                        <img src="assets/images/pegawai.png" heigth="100" width="100">
+                                        </div>
+                                        <div class="bold font-nm mb-3">Sistem Data Pegawai Atau Karyawan</div>
+                                        <div class="mb-4">
+                                            Sistem ini merupakan sistem untuk keperluan input data karyawan. Form pengisian data
+                                            karyawan bisa disesuaikan dengan keperluan client. Karyawan atau pegawai tersebut bisa dinilai dan hasilnya bisa digunakan
+                                            sebagai penilaian karyawan.
+                                        </div>
+                                        <div class="">
+                                            <button class="btn btn-outline-warning">Learn More</button>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 col-md-4 col-lg-3 mb-4">>
+                                        <div class="mt-3 mb-4">
+                                            <img src="assets/images/absensi.png" heigth="100" width="100">
+                                            </div>
+                                            <div class="bold font-nm mb-3">Sistem Absensi</div>
+                                            <div class="mb-4">
+                                                Sistem absensi disini yaitu terkait dengan keperluan kehadiran absensi anggota, siswa atau mahasiswa
+                                                yang nantinya akan terlihat seberapa besar persen kehadiran seseorang tersebut.
+                                            </div>
+                                            <div class="">
+                                                <button class="btn btn-outline-warning">Learn More</button>
+                                            </div>
+                                        </div>
+                                    </div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div  class="py-3">
+                        <div class="container">
+                            <div class="row bg-light">
+                                <div class="col-md-12 comp-grid">
+                                    <div class=""><h6 class=" text-left"> For business or ask DM to Instagram</h6>
+                                        <div >
+                                            <h6 class=" text-left"> <img src="assets/images/ig.png" heigth="25" width="25">  @glbalsolution</h6>
+                                            </div>
+                                            <div >
+                                                <h6 class=" text-left">  <img src="assets/images/ig.png" heigth="25" width="25"> @evan_fth</h6>
+                                                    <h6> <img src="assets/images/wa.png" height="25" width="25"> 082179371203 </h6>
+                                                    </div></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
